9daa40eb589e2dac5af83728194a71239cb73e4b
MissKittin <56042452+MissKittin@users.noreply.github.com>
2019-10-08 16:13:27 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php 
+	// PHP audio stream proxy
+	// 26.08.2019
+
+	error_reporting(E_ERROR | E_PARSE); //disable warnings
+
+	// Settings
+	$port=YOUR_SERVER_PORT; //server port
+	$username='YOUR_USERNAME'; //server login
+	$password='YOUR_PASSWORD'; //server password
+	$dyndns_server_data='PATH_TO_DYNDNS/ip.txt';
+
+	if(!$ip=file_get_contents($dyndns_server_data))
+	{ //something is wrong in config
+		?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Error</title>
+		<meta charset="utf-8">
+		<style type="text/css">
+			body {
+				background-color: #000;
+				color: #fff;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Server address not found</h1>
+	</body>
+</html>
+	<?php
+		exit();
+	}
+
+	$file='http://' . $username . ':' . $password . '@' . $ip . ':' . $port . '/stream.mp3';
+	if(!$head=array_change_key_case(get_headers($file, TRUE)))
+	{ //something is wrong with client
+		?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Error</title>
+		<meta charset="utf-8">
+		<style type="text/css">
+			body {
+				background-color: #000;
+				color: #fff;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Server is down. Sorry ¯\_(ツ)_/¯</h1>
+	</body>
+</html>
+	<?php
+		exit();
+	}
+
+	$size=$head['content-length']; // because filesize() won't work remotely
+	header('Content-Type: audio/mpeg');
+	header('Accept-Ranges: bytes');
+	header('Content-Disposition: inline');
+	//header('Content-Length:'.$size);
+
+	readfile($file);
+	exit();
+?>
